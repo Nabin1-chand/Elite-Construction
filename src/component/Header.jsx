a6dbd0c245a5299b130850567e56699bf1f0b96d
Nabin1-chand <nbc0117rdx@gmail.com>
2026-01-13 21:22:19 +0545

    added: folder structure in project

---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from '@chakra-ui/react';
+
+const Header = () => {
+  return (
+    <Box bg="blue.700" color="white" p={4}>
+      <Heading size="md">Construction Reporting System</Heading>
+    </Box>
+  );
+};
+
+export default Header;
