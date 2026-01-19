a9f0ade67645ffaab61280773c49c97cad719651
Nabin1-chand <nbc0117rdx@gmail.com>
2026-01-19 10:26:30 +0545

    code refactor

--- src/component/pages/showProjectCard.jsx
+++ src/component/pages/showProjectCard.jsx
@@ -1,12 +1,12 @@
 import Header from "../Header";
 import Sidebar from "../sidebar";
 import {
-    Box,
-    Text,
-    Heading,
-    Flex,
-    Button,
-    IconButton,
+  Box,
+  Text,
+  Heading,
+  Flex,
+  Button,
+  IconButton,
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { DeleteIcon } from "@chakra-ui/icons";
@@ -14,82 +14,82 @@ import { useContext } from "react";
 import { ProjectContext } from "../../provider/ProjectContext";
 const ProjectCard = (
 ) => {
-    const {projects, deleteProject} =useContext(ProjectContext);
-      const navigate = useNavigate();
-        const handleConfigure = (projectId) => {
+  const { projects, deleteProject } = useContext(ProjectContext);
+  const navigate = useNavigate();
+  const handleConfigure = (projectId) => {
     // navigate to create project page
     // optionally, you can pass projectId as state to edit later
     navigate("/projects/create", { state: { projectId } });
   };
-    return (
-        <Flex direction="column" minH="100vh" w="100vw">
-            <Header />
-            <Flex flex="1">
-                <Sidebar />
- <Flex wrap="wrap" gap={4} p={6} bg="gray.50">
+  return (
+    <Flex direction="column" minH="100vh" w="100vw">
+      <Header />
+      <Flex flex="1">
+        <Sidebar />
+        <Flex wrap="wrap" gap={4} p={6} bg="gray.50">
 
-         {projects.map((p) => (
-        <Box
-          key={p.id}
-          bg="white"
-          p={4}
-          mr={15}
-          rounded="md"
-          shadow="md"
-          w="300px"
-          h="300px"
-          display="flex"
-          flexDirection="column"
-          justifyContent="space-between"
-        >
-          <Box>
-            <Text fontWeight="bold" fontSize="lg">
-              {p.name}
-            </Text>
-            {/* <Text fontSize="sm" color="gray.600">
+          {projects.map((p) => (
+            <Box
+              key={p.id}
+              bg="white"
+              p={4}
+              mr={15}
+              rounded="md"
+              shadow="md"
+              w="300px"
+              h="300px"
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-between"
+            >
+              <Box>
+                <Text fontWeight="bold" fontSize="lg">
+                  {p.name}
+                </Text>
+                {/* <Text fontSize="sm" color="gray.600">
               Section: {p.client || "N/A"}
             </Text>          */}
-            <Box
-  p={3}
-  bg="white"
-  borderRadius="md"
-  boxShadow="sm"   // elevation
-  display="inline-block"
->
-  <Text fontSize="sm" color="gray.600">
-    Section: {p.client || "N/A"}
-  </Text>
-</Box>
-            <Text fontSize="sm" color="gray.600">
-              Holes: {p.numberOfHoles}
-            </Text>
-            <Text fontSize="sm" color="gray.600">
-              {p.description || "No description"}
-            </Text>
-          </Box>
+                <Box
+                  p={3}
+                  bg="white"
+                  borderRadius="md"
+                  boxShadow="sm"   // elevation
+                  display="inline-block"
+                >
+                  <Text fontSize="sm" color="gray.600">
+                    Section: {p.client || "N/A"}
+                  </Text>
+                </Box>
+                <Text fontSize="sm" color="gray.600">
+                  Holes: {p.numberOfHoles}
+                </Text>
+                <Text fontSize="sm" color="gray.600">
+                  {p.description || "No description"}
+                </Text>
+              </Box>
 
-          <Flex justify="space-between" mt={4}>
-            <Button size="sm" colorScheme="blue" onClick={() => handleConfigure(p.id)}>
-              Configure
-            </Button>
-            <IconButton
-              size="sm"
-              colorScheme="red"
-              icon={<DeleteIcon />}
-                onClick={() => deleteProject(p.id)}
+              <Flex justify="space-between" mt={4}>
+                <Button size="sm" colorScheme="blue" onClick={() => handleConfigure(p.id)}>
+                  Configure
+                </Button>
+                <IconButton
+                  size="sm"
+                  colorScheme="red"
+                  icon={<DeleteIcon />}
+                  onClick={() => deleteProject(p.id)}
 
-            />
-          </Flex>
-        </Box>
-        
-      ))}
+                />
+              </Flex>
+            </Box>
 
-</Flex>
-            </Flex>
+          ))}
+
         </Flex>
+      </Flex>
+    </Flex>
 
 
-    );
+  );
 };
 
 export default ProjectCard;
